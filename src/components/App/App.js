42d745ef9ed1ddfb9958b00c54be7f74c8395b23
Kong Yang <kpyang2414@gmail.com>
2021-07-25 14:46:22 -0500

    making sure image click goes to detail page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,24 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import MovieDetails from '../MovieDetails/MovieDetails';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
-        <Route path="/" exact>
+      <Router> 
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/details'>Movie Details</Link></li>
+          {/* Link to movies to details page to be deleted */}
+        </ul>       
+        <Route path='/' exact>
           <MovieList />
         </Route>
-        
+        <Route path='/details'>
+          <MovieDetails />
+        </Route>
         {/* Details page */}
 
         {/* Add Movie page */}
---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function MovieDetails() {
+
+    return (
+        <p>This should show details, page to be deleted</p>
+    )
+}
+
+export default MovieDetails;
